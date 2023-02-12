8533423621b6b79fd21540f8abb4f41db9c127e4
Alexis Arce <84654093+alexis-sudo@users.noreply.github.com>
2023-02-12 11:21:48 -0600

    update cliente

--- tortillasAdmin/src/client.js
+++ tortillasAdmin/src/client.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 //inicializamos la app con las credenciales que copiamos de firebase.
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyAlCfW2LLPMSKS3k3azQhw0be6DodB83-M",
-    authDomain: "tortillas-complete.firebaseapp.com",
-    projectId: "tortillas-complete",
-    storageBucket: "tortillas-complete.appspot.com",
-    messagingSenderId: "100818499736",
-    appId: "1:100818499736:web:e25f38a0c43df4bff478ed",
-    measurementId: "G-0WPGLEFL1L",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 });
 //asignamos la funcion a firestore
 const db = getFirestore();
